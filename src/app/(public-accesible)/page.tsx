0c91bfddc3d4cee5b5c7ace63612b2c66c2d2fac
CTRL <85201002+A-CTRLL@users.noreply.github.com>
2025-01-11 23:03:41 +0200

    reg and forgot password update

--- src/app/(public-accesible)/page.tsx
+++ src/app/(public-accesible)/page.tsx
@@ -8,7 +8,7 @@ import AuthCard from './_components/auth-card'
 
 function page() {
   return (
-    <div className='h-screen flex items-center justify-center bg-cover bg-[url(https://images.unsplash.com/photo-1485083269755-a7b559a4fe5e?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] auth-bg' >
+    <div className='h-screen flex items-center justify-center bg-cover bg-[url(https://images.unsplash.com/photo-1485083269755-a7b559a4fe5e?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)]' >
         <AuthCard>
           <h1 className='font-bold text-md text-center my-4'>Login</h1>
           <LoginForm/>
--- src/app/(public-accesible)/register/page.tsx
+++ src/app/(public-accesible)/register/page.tsx
@@ -8,9 +8,9 @@ import RegisterForm from '../_components/register-form'
 
 function page() {
   return (
-    <div className='h-screen flex items-center justify-center bg-gray-100'>
+    <div className='h-screen flex items-center justify-center bg-[url(https://images.unsplash.com/photo-1485083269755-a7b559a4fe5e?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)]'>
         <AuthCard>
-          <h1 className='font-bold text-md text-center my-4'>Forgot Password</h1>
+          <h1 className='font-bold text-md text-center my-4'>Register Password</h1>
           <RegisterForm/>
         </AuthCard>
     </div>
